1c39c99b47449d1727340453f8468ec0d4af22ae
조영욱 <cywibo@gmail.com>
2017-05-17 19:56:38 +0900

    intro

---
+++ app/src/main/java/com/examples/cleanproject/BackPressCloseHandler.java
@@ -0,0 +1,8 @@
+package com.examples.cleanproject;
+
+/**
+ * Created by Mac on 2017. 5. 17..
+ */
+
+public class BackPressCloseHandler {
+}
--- app/src/main/java/com/examples/cleanproject/IntroActivity.java
+++ app/src/main/java/com/examples/cleanproject/IntroActivity.java
@@ -1,13 +1,47 @@
 package com.examples.cleanproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class IntroActivity extends AppCompatActivity {
+    Handler handler;
 
+    SharedPreferences auto_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        handler = new Handler();
+        handler.postDelayed(mrun, 2000);
+    }
+
+    Runnable mrun = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent;
+            auto_login = getSharedPreferences("Clean", MODE_PRIVATE);
+            if(auto_login.getBoolean("Auto_login", false)){
+                intent = new Intent(IntroActivity.this, MainActivity.class);
+            }else{
+                intent = new Intent(IntroActivity.this, MainActivity.class);
+            }
+
+            startActivity(intent);
+            finish();
+
+        }
+    };
+    @Override
+    public void finish(){
+        super.finish();
+        this.overridePendingTransition(android.R.anim.fade_out, android.R.anim.fade_in);
+        /*화면 전환 애니메이션을 구현하기 위한 메소드 첫번째는 새로 띄워지는 액티비티에 적용할 애니메이션
+        두번째는 자신이 종료될 때 적용할 애니메이션
+         */
     }
 }
